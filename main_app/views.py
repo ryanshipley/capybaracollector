0b5107d97db87f41c018d6cf7555fa2bce3e0ed5
Ryan Shipley <ryanshipley23@gmail.com>
2022-10-19 12:40:19 -0400

    renders about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
