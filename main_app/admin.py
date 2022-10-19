cbd2d6bded9f09c8e6281341477a97eb4cdae2bf
Ryan Shipley <ryanshipley23@gmail.com>
2022-10-19 19:08:35 -0400

    adds cleaning model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Capybara
+from .models import Capybara, Cleaning
 # Register your models here.
 admin.site.register(Capybara)
+admin.site.register(Cleaning)
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+CLEAN = (
+    ('T', 'Teeth'),
+    ('H', 'Hair'),
+    ('N', 'Nails')
+)
+
 # Create your models here.
 class Capybara(models.Model):
     name = models.CharField(max_length=50)
@@ -7,3 +13,12 @@ class Capybara(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     image = models.TextField(max_length=500)
+
+class Cleaning(models.Model):
+    date = models.DateField('cleaning date')
+    clean = models.CharField(max_length=1, choices=CLEAN, default=CLEAN[0][0])
+
+    capybara = models.ForeignKey(Capybara, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_clean_display()} on {self.date}"
